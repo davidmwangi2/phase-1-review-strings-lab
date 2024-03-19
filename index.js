7d4e8482030f531fb49238bdc440e4ca417e61ae
david <david.mwangi2@student.moringaschool.com>
2024-03-19 21:00:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, Welcome to Flatbook, Grace Hopper !';
+const excitedWelcomeMessage = 'Welcome to Flatbook, Grace Hopper, GRACE HOPPER, WELCOME TO FLATBOOK, WELCOME TO FLATBOOK!';
+const shortGreeting = 'Welcome, Grace !';
+const firstInitial = 'Grace Hopper';
+const restOfName = 'Grace Hopper';
+
